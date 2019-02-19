de1ebd9dfc168375888d9a745e79f17af0a5d47a
Taieb <taiebalfalah@gmail.com>
2019-02-19 10:31:08 +0100

    First Commit
    :D

---
+++ algo.js
@@ -0,0 +1,21 @@
+//Write a loop that makes seven calls to console.log to output the following triangle
+
+let x = 0
+let y='#"
+res =''
+while ( x < 9 ) {
+x ++
+res += y
+console.log(res)
+}
+
+/*
+expected result 
+#
+##
+###
+####
+#####
+######
+#######
+*/
