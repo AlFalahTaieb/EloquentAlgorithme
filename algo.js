4ffd9da3df6b9760f7ff9bc781096f6d8e49f4ad
Taieb <taiebalfalah@gmail.com>
2019-02-19 10:50:31 +0100

    FizzBuzz Algo

--- algo.js
+++ algo.js
@@ -19,3 +19,24 @@ expected result
 ######
 #######
 */
+
+
+// FizzBuzz Algorithm
+
+let y = 3
+let x = 5 
+let z=0
+
+while (z<100){
+z++
+if ( z % 3 == 0 && z % 5 == 0 ) {
+console.log('FizzBuzz')
+}else if ( z % 5 == 0 ) {
+console.log('Buzz')
+}else if ( z % 3 == 0 ){
+console.log('Fizz')
+}
+else console.log(z)
+}
+
+
