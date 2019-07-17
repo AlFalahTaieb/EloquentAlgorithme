ff32af9bc3990df2bdb7cfe7a486117272ea534d
AlFalah <taiebalfalah@gmail.com>
2019-07-17 16:31:38 +0200

    12. Integer to Roman

--- algo.js
+++ algo.js
@@ -647,3 +647,27 @@ let numberPalindrome = function(x) {
     } return x==reversedX ? true : false  
 }
 numberPalindrome(1)
+
+
+//12. Integer to Roman https://leetcode.com/problems/integer-to-roman/
+intToRoman=(num)=> {
+    const hash = [
+        ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+        ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+        ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+        ['', 'M', 'MM', 'MMM']
+    ]
+    
+    let result = ''
+    let i = 0
+    while (num > 0) {
+      console.log(result)
+    
+        result = hash[i++][num % 10] + result
+        
+        num = Math.floor(num / 10)
+    }
+    
+    return result
+}
+intToRoman(1778)
